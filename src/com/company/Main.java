0914ad2bd0fd9702cf64fdfa6db310fd82bfac3a
sandr655 <62062307+sandr655@users.noreply.github.com>
2022-01-18 19:08:18 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String town = "Detroid";
+        int population = 100_000;
+        float unemployed = 23.5f;
+        char section = 'B';
+        long place = 15;
+        short bilet = 3000;
+        String name = "Karl";
+        byte age = 25;
+        short height = 187;
+        char numCar = 'A';
+        byte numCar2 = 0;
+        double veghtCar = 2000.55;
+        String nameCompany = "Alibaba";
+        long dohod = 8_000_000_000l;
+        byte doly = 55;
+        char rezus = '-';
+        byte gruppa = 3;
+        short dolyPeople = 33;
+        String univer = "Plehanova";
+        int numPeople = 65000;
+        byte dolyOt = 10;
+        char reyting = '2';
+        long numKopiy = 7685890;
+        short numR = -545;
+        String brend = "Lotus";
+        long numProduct = 789798254324l;
+        byte sales = 30;
+        char catigoria = 'M';
+        long kod = 19020934324l;
+        byte buyers = 25;
+        System.out.println("1) "+town + " "+population+" "+unemployed);
+        System.out.println("2) "+section + " "+place+" "+bilet);
+        System.out.println("3) "+name + " "+age+" "+height);
+        System.out.println("4) "+numCar+numCar2+numCar2+numCar2+numCar+numCar+" "+veghtCar);
+        System.out.println("5) "+nameCompany + " "+dohod+" "+doly);
+        System.out.println("6) "+rezus + " "+gruppa+" "+dolyPeople);
+        System.out.println("7) "+univer + " "+numPeople+" "+dolyOt);
+        System.out.println("8) "+reyting + " "+numKopiy+" "+numR);
+        System.out.println("9) "+brend + " "+numProduct+" "+sales);
+        System.out.println("10) "+catigoria + " "+kod+" "+buyers);
+    }
+}
